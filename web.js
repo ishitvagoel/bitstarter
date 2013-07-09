5a9b99c7bb44a77c40a63ab9a862a5f854051802
Ishitva Goel <ishitva.goel@yahoo.co.in>
2013-07-09 22:51:13 +0530

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var str= fs.readFileSync('index.html');
+  var buffer = new Buffer(str);
+	
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
